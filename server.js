f0be2aa49f92b78d1b61372d81f7bb706683f297
osazeejedi <oghagbonosazee20@gmail.com>
2025-03-04 15:21:58 +0100

    update

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-// Load environment variables first
-require('dotenv').config();
+// Load environment variables if .env exists, but don't fail if it doesn't
+try {
+  require('dotenv').config();
+} catch (err) {
+  console.log('No .env file found, using environment variables from Render');
+}
 
 // Then require the server
 require('./dist/src/server'); 
